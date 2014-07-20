425c3dacb47a340c0339dbcd89bf9747e144bd4d
unknown <gayan.pathirana@VAN1146BRGAYANPC.na.corp.yr.com>
2014-07-20 16:31:43 -0700

    Minor clean up. Made the Twitter controller the default controller.

--- pbpTwitterExercise/App_Start/RouteConfig.cs
+++ pbpTwitterExercise/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace pbpTwitterExercise
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Twitter", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
